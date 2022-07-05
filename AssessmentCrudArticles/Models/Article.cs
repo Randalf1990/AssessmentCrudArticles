4a2bd21f1144105cd47372e5c583c1ddb913191c
randa <randa@DESKTOP-L84A15N>
2022-07-05 17:04:05 -0500

    Agregar archivos de proyecto.

---
+++ AssessmentCrudArticles/Models/Article.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssessmentCrudArticles.Models
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        public string Author { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        [MaxLength(1000)]
+        public string Body { get; set; }
+    }
+}
---
+++ AssessmentCrudArticles/Repositories/AssessmentContext.cs
@@ -0,0 +1,15 @@
+﻿using AssessmentCrudArticles.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssessmentCrudArticles.Repositories
+{
+    public class AssessmentContext : DbContext
+    {
+        public AssessmentContext(DbContextOptions<AssessmentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Article> Users { get; set; }
+    }
+}
